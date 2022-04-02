14af8b84c34c69683c82a0e7a1520656ff9c97c6
J2mF <mp4-12cs5@hotmail.com>
2022-04-02 21:51:47 +0300

    Implement LSFR

---
+++ src/main/java/org/thekiddos/a5/BaseLSFR.java
@@ -0,0 +1,11 @@
+package org.thekiddos.a5;
+
+import java.util.BitSet;
+
+public interface BaseLSFR {
+    void initialize(BitSet sessionKey, BitSet frameCounter);
+    boolean clock();
+    // TODO: make dynamic
+    int SESSION_KEY_LENGTH = 64;
+    int FRAME_COUNTER_LENGTH = 22;
+}
