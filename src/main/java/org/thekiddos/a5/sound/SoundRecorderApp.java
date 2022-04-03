d4420133ec54c8ceacd75183f6031d487fa131b5
J2mF <mp4-12cs5@hotmail.com>
2022-04-03 17:02:39 +0300

    Add Sound Recorder from baeldung

---
+++ src/main/java/org/thekiddos/a5/sound/SoundRecorderApp.java
@@ -0,0 +1,34 @@
+package org.thekiddos.a5.sound;
+
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
+
+class SoundRecorderApp {
+    public static void main(String[] args) throws Exception {
+
+        AudioFormat format = buildAudioFormatInstance();
+
+        SoundRecorder soundRecorder = new SoundRecorder();
+        soundRecorder.build(format);
+
+        System.out.println("Start recording ....");
+        soundRecorder.start();
+        Thread.sleep(3000);
+        soundRecorder.stop();
+
+        WaveDataUtil wd = new WaveDataUtil();
+        Thread.sleep(3000);
+        wd.saveToFile("SoundClip", AudioFileFormat.Type.WAVE, soundRecorder.getAudioInputStream());
+    }
+
+    public static AudioFormat buildAudioFormatInstance() {
+        ApplicationProperties aConstants = new ApplicationProperties();
+        AudioFormat.Encoding encoding = aConstants.ENCODING;
+        float rate = aConstants.RATE;
+        int channels = aConstants.CHANNELS;
+        int sampleSize = aConstants.SAMPLE_SIZE;
+        boolean bigEndian = aConstants.BIG_ENDIAN;
+
+        return new AudioFormat(encoding, rate, sampleSize, channels, (sampleSize / 8) * channels, rate, bigEndian);
+    }
+}
