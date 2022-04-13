b3865472f396146d7329411778d8a5b861199754
J2mF <mp4-12cs5@hotmail.com>
2022-04-13 22:41:56 +0300

    Add jar main class

---
+++ src/main/java/org/thekiddos/a5/Main.java
@@ -0,0 +1,11 @@
+package org.thekiddos.a5;
+
+import org.thekiddos.a5.gui.A5VoiceGUI;
+
+
+// Used as a hack for including javaFX in jar
+public class Main {
+    public static void main( String[] args ) {
+        A5VoiceGUI.main( args );
+    }
+}
